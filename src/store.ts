4c8e5e9f270b91bc763eba1aa8adfbf45b045510
Tyler Long <tyler4long@gmail.com>
2020-08-19 22:35:17 -0700

    Chatbot access token

--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,19 @@
 import SubX from 'subx';
 import RingCentral from '@rc-ex/core';
+import Rest from '@rc-ex/core/lib/Rest';
 
 export type StoreType = {
   ready: boolean;
   invite: Function;
 };
 
-const rc = new RingCentral({
-  clientId: process.env.RINGCENTRAL_CLIENT_ID,
-  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
-  server: process.env.RINGCENTRAL_SERVER_URL,
-});
+const rc = new RingCentral({server: Rest.productionServer});
 
 const store = SubX.proxy<StoreType>({
   ready: false,
   invite: (email: string) => {
     console.log(`invite ${email}`);
+    console.log('access token', process.env.RINGCENTRAL_ACCESS_TOKEN);
   },
 });
 
