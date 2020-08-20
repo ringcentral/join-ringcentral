aa666928514d7079daf6acdb9ac1521189a49e1a
Tyler Long <tyler4long@gmail.com>
2020-08-19 20:17:19 -0700

    Form validation

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Component} from 'react-subx';
-import {Input, Button} from 'antd';
+import {Input, Button, Form} from 'antd';
 
 import {StoreType} from './store';
+import {FormInstance} from 'antd/lib/form';
 
 type PropsStore = {
   store: StoreType;
@@ -11,23 +12,46 @@ type StateType = {
   email: string;
 };
 class App extends Component<PropsStore, StateType> {
+  form: React.RefObject<FormInstance>;
   constructor(props: PropsStore) {
     super(props);
     this.state = {email: ''};
+    this.form = React.createRef<FormInstance>();
   }
   render() {
     const store = this.props.store;
     return (
-      <>
-        <Input
-          placeholder="Email Address"
-          type="email"
-          onChange={e => this.setState({email: e.target.value})}
-        />
-        <Button type="primary" onClick={() => store.invite(this.state.email)}>
-          Invite
-        </Button>
-      </>
+      <Form ref={this.form}>
+        <Form.Item
+          rules={[
+            {required: true, message: 'Please input your email!'},
+            {type: 'email', message: 'Invalid email address!'},
+          ]}
+          name="email"
+        >
+          <Input
+            placeholder="Email Address"
+            type="email"
+            onChange={e => this.setState({email: e.target.value})}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            type="primary"
+            onClick={() => {
+              if (
+                !this.form
+                  .current!.getFieldsError()
+                  .some(i => i.errors.length > 0)
+              ) {
+                store.invite(this.state.email);
+              }
+            }}
+          >
+            Invite
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
